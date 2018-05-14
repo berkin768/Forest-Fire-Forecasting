0963c9bf06909479fe9a74e21cfbdff0e8e6bf11
Dogancan <dogancancolak1@gmail.com>
2018-05-15 00:04:43 +0300

    Cross-validation added to both classifiers

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report, confusion_matrix, make_scorer
 
 
-def dataPreperation(data):
-    return data
-
-
 def readData():
     folderPath = os.getcwd()
     filePath = folderPath + '/dataset/' + 'forestfires.csv'
@@ -21,8 +17,6 @@ def readData():
 def getFeatures(dataset):
     X = dataset.loc[:, dataset.columns != 'area']
     Y = dataset[['area']]
-    # Y.ix[Y.area > 0, 'area'] = 1
-    # Y.ix[Y.area < 0, 'area'] = 0
     Y.is_copy = False
     Y.loc[Y['area'] > 0, 'area'] = '1'
     Y.loc[Y['area'] == 0, 'area'] = '0'
@@ -39,7 +33,7 @@ def DecisionTree(descriptiveFeatures, targetFeature):
 
     clf = DecisionTreeClassifier()
     scores = cross_validate(clf, descriptiveFeatures, targetFeature, scoring=scoring,
-                            cv=5, return_train_score=True)
+                            cv=5, return_train_score=False)
     print(scores)
 
 
@@ -48,20 +42,13 @@ def NeuralNetworkClassifier(descriptiveFeatures, targetFeature):
 
     targetFeature = targetFeature.apply(le.fit_transform)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        descriptiveFeatures, targetFeature, test_size=0.2)
-    scaler = StandardScaler()
-    scaler.fit(X_train)
-
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
+    scoring = {'acc': 'accuracy',
+               'prec_macro': 'precision_macro',
+               'rec_micro': 'recall_macro'}
     mlp = MLPClassifier(hidden_layer_sizes=(29, 20, 1), max_iter=1000)
-    mlp.fit(X_train, y_train.values.ravel())
-    prediction = mlp.predict(X_test)
-    """cv = KFold(n_splits=3)
-    prediction = cross_val_predict(mlp, X_train, y_train, cv=cv)"""
-    print(confusion_matrix(y_test, prediction))
-    print(classification_report(y_test, prediction))
+    scores = cross_validate(mlp, descriptiveFeatures, targetFeature, scoring=scoring,
+                            cv=5, return_train_score=False)
+    print(scores)
 
 
 def main():
@@ -70,10 +57,10 @@ def main():
 
     # print(dataset)
     descriptiveFeatures, targetFeature = getFeatures(dataset)
-    """with pd.option_context('display.max_rows', None, 'display.max_columns', 30):
-        print(targetFeature)"""
 
-    # NeuralNetworkClassifier(descriptiveFeatures, targetFeature)
+    print("NEURAL NETWORK : ", end="\n\n")
+    NeuralNetworkClassifier(descriptiveFeatures, targetFeature)
+    print("DECISION TREE : ", end="\n\n")
     DecisionTree(descriptiveFeatures, targetFeature)
 
 

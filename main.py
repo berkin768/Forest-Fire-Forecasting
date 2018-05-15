8cfd481ca63d540fb91ae8b89a74560c60faa6a1
Berkin Akaydın <berkinnakaydinn@gmail.com>
2018-05-15 20:46:36 +0300

    Added Plots

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report, confusion_matrix, make_scorer
-
-
+import matplotlib.pyplot as plt
+from statistics import mean
 import warnings
 warnings.filterwarnings('ignore')
-
 
 def readData():
     folderPath = os.getcwd()
     filePath = folderPath + '/dataset/' + 'forestfires.csv'
     dataset = pd.read_csv(filePath)
     return dataset
-
 
 def getFeatures(dataset):
     X = dataset.loc[:, dataset.columns != 'area']
@@ -25,7 +23,6 @@ def getFeatures(dataset):
     Y.loc[Y['area'] > 0, 'area'] = '1'
     Y.loc[Y['area'] == 0, 'area'] = '0'
     return X, Y
-
 
 def DecisionTree(descriptiveFeatures, targetFeature):
     le = LabelEncoder()
@@ -39,10 +36,19 @@ def DecisionTree(descriptiveFeatures, targetFeature):
     scores = cross_validate(clf, descriptiveFeatures, targetFeature, scoring=scoring,
                             cv=5, return_train_score=False)
 
-    for key,val in scores.items():
-        print(key, end=' : ')
-        print(val)
+    names = list()
+    values = list()
 
+    for key,val in scores.items():
+        names.append(key)
+        values.append(mean(val))
+        print(key, end=' : ')
+        print(mean(val))
+    
+    plt.figure(figsize=(10,7))
+    plt.bar(range(len(names)),values,tick_label=names)
+    plt.title('Decision Tree')
+    plt.show()
 
 def NeuralNetworkClassifier(descriptiveFeatures, targetFeature):
     le = LabelEncoder()
@@ -58,11 +64,22 @@ def NeuralNetworkClassifier(descriptiveFeatures, targetFeature):
     scores = cross_validate(mlp, descriptiveFeatures, targetFeature, scoring=scoring,
                             cv=5, return_train_score=False)
 
-    for key,val in scores.items():
-        print(key, end=' : ')
-        print(val)
-    
 
+    names = list()
+    values = list()
+
+    for key,val in scores.items():
+        names.append(key)
+        values.append(mean(val))
+        print(key, end=' : ')
+        print(mean(val))
+    
+    plt.figure(figsize=(10,7))
+    plt.bar(range(len(names)),values,tick_label=names)
+    plt.title('Neural Network')
+    plt.show()
+       
+    
 def main():
     rawDataset = readData()
     dataset = pd.get_dummies(rawDataset)
@@ -74,7 +91,6 @@ def main():
     print(end="\n")
     print("DECISION TREE : ", end="\n")
     DecisionTree(descriptiveFeatures, targetFeature)
-
 
 if __name__ == '__main__':
     main()
